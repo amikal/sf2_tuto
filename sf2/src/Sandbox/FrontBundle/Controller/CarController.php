95aee5bfdb5d399e495ea883125e2ed282d0ac04
David K. aka amikal <edk4testtools@gmail.com>
2017-10-15 17:06:58 +0200

    + entity, repository, find entity

--- sf2/src/Sandbox/FrontBundle/Controller/CarController.php
+++ sf2/src/Sandbox/FrontBundle/Controller/CarController.php
@@ -3,6 +3,8 @@
 namespace Sandbox\FrontBundle\Controller;
 
 
+use Doctrine\ORM\EntityNotFoundException;
+use Sandbox\BackBundle\Entity\Car;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
@@ -22,13 +24,20 @@ class CarController extends Controller
         /* @var $session Session */
         $session = $this->get('session');
         $session->set('userName', 'John');
-        dump($session);
+        //dump($session);
 
-        $session->getFlashBag()->add('errors_info', 'no_error_here');
-        $session->getFlashBag()->add('errors_info', 'no_error_here2');
-        $session->getFlashBag()->add('errors_info', 'no_error_here3');
+//        $session->getFlashBag()->add('errors_info', 'no_error_here');
+//        $session->getFlashBag()->add('errors_info', 'no_error_here2');
+//        $session->getFlashBag()->add('errors_info', 'no_error_here3');
 
-        return $this->render('SandboxFrontBundle:Car:car.html.twig', ['id' => $id]);
+        $repository = $this->getDoctrine()->getManager()->getRepository('SandboxBackBundle:Car');
+        $car = $repository->find($id);
+
+        if (!$car) {
+            throw new EntityNotFoundException();
+        }
+
+        return $this->render('SandboxFrontBundle:Car:car.html.twig', ['car' => $car]);
     }
 
     /**
@@ -40,6 +49,16 @@ class CarController extends Controller
 //       return $this->redirect($url);
 //
 //       return $this->forward('SandboxFrontBundle:Car:Car', ['id' => 3]);
+
+//       $em = $this->getDoctrine()->getManager();
+//       $car = new Car();
+//       $car->setName('Twingo');
+//       $car->setMarque('Renault');
+//       $car->setType('citadine');
+//       $car->setCreated(new \DateTime('now'));
+//
+//       $em->persist($car);
+//       $em->flush();
 
        return $this->render('SandboxFrontBundle:Car:carList.html.twig');
     }
